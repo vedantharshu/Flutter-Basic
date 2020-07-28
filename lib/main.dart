91dd50cce95a4b06a3c98ba4961fdd23b639500d
vedantharshu <vedantharbu@gmail.com>
2020-07-29 00:27:08 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(
+        home: Basics(),
+  ));
+}
+
+class Basics extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Basics> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[100],
+      appBar: AppBar(
+        title: Text('Flutter Basics App'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textBaseline: TextBaseline.alphabetic,
+          children: <Widget>[
+            Text(
+              'hello world',
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 20.0,
+              ),
+            ),
+            FlatButton.icon(onPressed: () {}, icon:Icon(Icons.email), label: Text('Click Me')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/cena.jpg'),
+                  radius: 80.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(18.0,0.0,0.0,0.0),
+                  child: Text('Vedant'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
